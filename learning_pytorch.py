bf57ddc107f2ef715c1617fca9030f9598472604
akavall <akavall@gmail.com>
2020-05-23 18:28:50 -0700

    started to add pytorch agent, basic version looks to be working, not tested

---
+++ learning_pytorch.py
@@ -0,0 +1,101 @@
+
+import numpy as np
+
+from zombie_dice import Player, init_player_state, init_game_state
+from pytorch_agent import PyTorchAgent
+
+from collections import deque
+
+from tests import test_model
+
+
+def get_features(player, game_state):
+    s = player.player_state 
+    state = []
+    state.append(s.current_score)
+    state.append(s.times_shot)
+    state.append(s.brains_rolled)
+    state.append(s.walks_taken_last_roll)
+    # state.append(s.is_dead)
+
+    dices = game_state.zombie_deck.dices 
+
+    greens = sum(x.name == "green" for x in dices)
+    yellows = sum(x.name == "yellow" for x in dices)
+    reds = sum(x.name == "red" for x in dices)
+
+    if len(dices) >= 3:
+        state.append(greens)
+        state.append(yellows)
+        state.append(reds)
+    else:
+        state.append(greens + 6)
+        state.append(yellows + 4)
+        state.append(reds + 3)
+
+    if s.is_dead:
+        reward = 0
+    else:
+        reward = s.brains_rolled
+
+    return state, reward, s.is_dead
+
+
+def learn(agent, n_episodes, maxlen_scores):
+
+    scores = deque(maxlen=maxlen_scores)
+
+    for e in range(n_episodes):
+
+        print(f"On episode: {e}")
+
+        player_a = Player(init_player_state(), "a", False, 0)
+        game_state = init_game_state([player_a], "my_game")
+        state, reward, done = get_features(player_a, game_state)
+        state = np.expand_dims(state, axis=0)
+        done = False
+
+        while done is False:
+
+            action = agent.act(state)
+            if action == 0:
+                done = True
+                new_state = player_a.player_state.brains_rolled
+                reward = new_state
+            else:
+                player_a.take_turn(game_state.zombie_deck)
+                new_state, reward, done = get_features(player_a, game_state)
+                reward = 0
+                if done:
+                    new_state = reward # reward is 0 here
+                else:
+                    new_state = np.expand_dims(new_state, axis=0)
+
+            agent.remember((state, action, reward, new_state, done))
+
+            state = new_state
+
+            if done:
+                agent.update_policy_weights()
+                break
+
+            agent.replay_memory()
+
+        scores.append(new_state)
+        print(f"score last 100 avg: {np.mean(scores)}, current_score: {new_state}")
+
+        # model_score = test_model(agent.model)
+        # print(f"model_score: {model_score}")
+
+    # We need this for testing
+
+    # We need this to load model to golang
+
+
+if __name__ == "__main__":
+
+    pytorch_agent = PyTorchAgent(input_size=7, action_size=2)
+
+    learn(agent=pytorch_agent, n_episodes=1000, maxlen_scores=100)
+
+    # might want to save the model 
---
+++ pytorch_agent.py
@@ -0,0 +1,120 @@
+
+import torch 
+from torch import nn 
+
+import torch.nn.functional as F
+
+import numpy as np
+import random as rn
+
+from collections import deque 
+
+class RNNRegressor(nn.Module):
+    def __init__(self, input_size, action_size):
+        super(RNNRegressor, self).__init__()
+
+        self.fc1 = nn.Linear(input_size, 32)
+        self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, action_size)
+
+    def forward(self, input):
+
+
+
+        fc_1_output = F.relu(self.fc1(input))
+        fc_2_output = F.relu(self.fc2(fc_1_output))
+        fc_3_output = self.fc3(fc_2_output)
+
+        return fc_3_output
+
+class PyTorchAgent:
+
+    def __init__(self, 
+                 input_size, 
+                 action_size, 
+                 batch_size=32,
+                 memories_capacity=1000):
+        self.input_size = input_size
+        self.action_size = action_size 
+        self.model = RNNRegressor(self.input_size, self.action_size)
+        self.target_model = RNNRegressor(self.input_size, self.action_size)
+        self.criterion = nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
+        self.memories = deque([], memories_capacity)
+        self.batch_size = batch_size 
+        self.epsilon=0.95
+        self.epsilon_decay=0.995
+        self.epsilon_min=0.01
+        self.discount_rate=0.95
+
+    def remember(self, memory):
+        self.memories.append(memory)
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return rn.randrange(self.action_size)
+
+        state_torch = torch.from_numpy(state).float()
+        act_values = self.model(state_torch)
+
+        return act_values.argmax()
+
+    def replay_memory(self):
+
+        if len(self.memories) < self.batch_size:
+            return 
+
+        sample_memories = rn.sample(self.memories, self.batch_size)
+
+        for state, action, reward, new_state, done in sample_memories:
+
+            self.optimizer.zero_grad()
+
+            state_pytorch = torch.from_numpy(state).float()
+            new_state_pytorch = torch.from_numpy(state).float()
+            
+            action_scores = self.model(state_pytorch)
+            action_scores_original = action_scores
+            # check on dimensions
+
+            if done:
+                action_scores[0][action] = reward
+            else:
+                if isinstance(new_state, int):
+                    new_action_score = new_state
+                else:
+                    temp = self.target_model(new_state_pytorch)
+                    new_action_score = temp.max()
+
+                action_scores[0][action] = reward + self.discount_rate * new_action_score
+
+            loss = self.criterion(action_scores_original, action_scores)
+            self.optimizer.step()
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def update_policy_weights(self):
+    
+        with torch.no_grad():
+            self.target_model.fc1.weight = self.model.fc1.weight 
+            self.target_model.fc2.weight = self.model.fc2.weight 
+
+
+
+
+
+
+
+
+
+            
+
+
+
+    
+
+
+
+
+
